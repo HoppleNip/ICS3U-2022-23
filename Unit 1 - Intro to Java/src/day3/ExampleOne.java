0bb5b25a3265aa526aeec389096158c1f36e48dc
Kai Shaw <kshaw@bayviewglen.ca>
2022-09-12 15:05:43 -0400

    varibles

---
+++ Unit 1 - Intro to Java/src/day3/ExampleOne.java
@@ -0,0 +1,36 @@
+package day3;
+
+public class ExampleOne {
+    public static void main(String[] args) {
+        // primitive Data Types
+        // Variables allow us to store data/information
+        //we must delcare the varible and the type od data it will store
+
+        //primitie data types
+        //int -> integer (have no decimal portion)
+        //double -> decimal numbers
+        //boolean -> true/false
+
+        //naming convention for variables (camelCase)
+
+        int numberOfStudents = 17; // = 
+        double averageAge = 18.3;
+        
+        System.out.println(numberOfStudents);
+        System.out.println(averageAge);
+        System.out.println(5+3);
+
+        //good practiuce declare varib,es as you need themin
+        int numberOne, numberTwo, numberThree; //you can delcan multuple varibles of the same type on the3 same line
+
+        numberOne = 10;
+        numberTwo = 16;
+        numberThree = 11;
+
+        int sum = numberOne + numberTwo + numberThree;
+
+        System.out.println("THe sum is " + sum);  // string concatenation
+
+
+    }
+}
